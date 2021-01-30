bef915bc9ca6a8587fa970c9f0a469805020b005
Johnny Goth <Jgoth23@gmail.com>
2021-01-30 15:02:05 -0600

    started some functions in js

--- salary.js
+++ salary.js
@@ -9,8 +9,24 @@ function readyNow() {
   //Save employee info to array
   $(document).on('click', '#addEmployeeBtn', onAddEmployee);
 }
-
+/**This function needs to grab input from the Dom
+ * Creat in employee object
+ * Push employee info into an array
+ */
 function onAddEmployee(event) {
   event.preventDefault();
   console.log('onAddEmployee');
+  let firstName = $('#firstNameInput');
+  let lastName = $('#lastNameInput');
+  let employeeID = $('#employeeIDInput');
+  let title = $('#employeeTitleInput');
+  let annualSalary = $('#annualSalaryInput');
+
+  let employeeInfo = {
+    firstName: firstName,
+    lastName: lastName,
+    employeeID: employeeID,
+    title: title,
+    annualSalary: Number(annualSalary),
+  };
 }
